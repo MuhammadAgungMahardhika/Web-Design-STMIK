df080c726da1aa787b80fe4d6836400f625b96ae
MuhammadAgungMahardhika <58037995+MuhammadAgungMahardhika@users.noreply.github.com>
2022-03-18 13:13:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
         <section id="about" >
             <div class="container">
                 <div class="row row-about">
-                    <h1 class="judul_about ">Tentang STIMIK Indonesia</h1>
+                    <h1 class="judul_about texts ">Tentang STIMIK Indonesia</h1>
                     <div class="block texts"></div>
                     <p class="texts" style="text-align:justify">Saat ini Institusi dan Program Studi STMIK Indonesia telah terakreditasi B. Visi dari STMIK Indonesia Padang adalah menjadi salah satu perguruan tinggi di bidang komputer yang terkemuka di ASEAN pada tahun 2033. Sedangkan misinya antara lain mendidik dan membina mahasiswa menjadi tenaga ahli yang profesional di bidangnya, berjiwa kewirausahaan, dan berperilaku Islami serta menghasilkan lulusan yang berkualitas, kreatif, inovatif dan berdaya saing Internasional. Untuk visi Prodi S1 Sistem Informasi adalah terkemuka ditingkat Nasional tahun 2023 bidang system analyst dan database administrator yang profesional.</p>
                 </div>
@@ -93,12 +93,14 @@
 
         <!-- fasilitas Section -->
 
-        <section id="team">
+        <section id="team" style="margin-bottom: -10px;">
             <div class="container" >
                 <h1 class="stimik fasilitas">Fasilitas</h1>
                 <div class="block fasilitas"></div>
               
             </div>
+
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" ><path fill="#687B7E" fill-opacity="1" d="M0,128L10.4,138.7C20.9,149,42,171,63,149.3C83.5,128,104,64,125,58.7C146.1,53,167,107,188,122.7C208.7,139,230,117,250,96C271.3,75,292,53,313,85.3C333.9,117,355,203,376,197.3C396.5,192,417,96,438,48C459.1,0,480,0,501,26.7C521.7,53,543,107,563,117.3C584.3,128,605,96,626,96C647,96,668,128,689,138.7C709.6,149,730,139,751,165.3C772.2,192,793,256,814,277.3C834.8,299,856,277,877,266.7C897.4,256,918,256,939,213.3C960,171,981,85,1002,69.3C1022.6,53,1043,107,1064,117.3C1085.2,128,1106,96,1127,117.3C1147.8,139,1169,213,1190,218.7C1210.4,224,1231,160,1252,138.7C1273,117,1294,139,1315,138.7C1335.7,139,1357,117,1377,138.7C1398.3,160,1419,224,1430,256L1440,288L1440,320L1429.6,320C1419.1,320,1398,320,1377,320C1356.5,320,1336,320,1315,320C1293.9,320,1273,320,1252,320C1231.3,320,1210,320,1190,320C1168.7,320,1148,320,1127,320C1106.1,320,1085,320,1064,320C1043.5,320,1023,320,1002,320C980.9,320,960,320,939,320C918.3,320,897,320,877,320C855.7,320,835,320,814,320C793,320,772,320,751,320C730.4,320,710,320,689,320C667.8,320,647,320,626,320C605.2,320,584,320,563,320C542.6,320,522,320,501,320C480,320,459,320,438,320C417.4,320,397,320,376,320C354.8,320,334,320,313,320C292.2,320,271,320,250,320C229.6,320,209,320,188,320C167,320,146,320,125,320C104.3,320,83,320,63,320C41.7,320,21,320,10,320L0,320Z"></path></svg>
         </section>
 
       
@@ -108,8 +110,9 @@
             <div class="container">
                 <div class="quoteLoop">
                     <blockquote class="quote">
-                        <img src="https://placehold.it/100x100?text=First Client" width="100" height="100" alt=""/>
-                        <h5>&nbsp;<br>
+                        <img src="images/STMIK_logo.png" width="90" height="90" alt=""/>
+                        <p style="color: #eaeaea;">STIMIK Indonesia</p>
+                        <h5 style="color: white;">&nbsp;<br>
                             &rdquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.&rdquo;<br>
                             <small>Steve Bruce, Sed ut perspiciatis unde omnis</small>
                         </h5>
