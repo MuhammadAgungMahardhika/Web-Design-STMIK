22b3c5a406c5dfde925720936c96e6ed90164b5b
MuhammadAgungMahardhika <58037995+MuhammadAgungMahardhika@users.noreply.github.com>
2022-03-18 11:29:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -31,8 +31,7 @@
                             <li><a class="smoothscroll" href="#header">Beranda</a></li>
                             <li><a class="smoothscroll" href="#about">Tentang</a></li>
                             <li><a class="smoothscroll" href="#team">Fasilitas</a></li>
-                            <li><a class="smoothscroll" href="#contact">Kontak</a></li>
-
+                           
                         </ul>
                        
                     </nav>
@@ -176,31 +175,7 @@
             </div>
         </section>
 
-        <!-- Contact Section -->
-
-        <section id="contact">
-            <div class="container">
-                <h1>Contact</h1>
-                <div class="block"></div>
-                <form>
-                    <div class="row">
-                        <div class="six columns">
-                            <label for="exampleRecipientInput">Name</label>
-                            <input class="u-full-width" type="text">
-                        </div>
-                        <div class="six columns">
-                            <label for="exampleEmailInput">Email</label>
-                            <input class="u-full-width" type="email">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <label for="exampleMessage">Message</label>
-                        <textarea class="u-full-width"></textarea>
-                        <input class="button-primary" type="submit" value="Submit">
-                    </div>
-                </form>
-            </div>
-        </section>
+      
 
         <!-- Footer Section -->
 
